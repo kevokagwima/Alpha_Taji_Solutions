b77c3372bdfc4392f945c00eb6ef3e0a7304f49c
Kevo Kagwima <kevokagwima@gmailcom>
2021-04-14 11:57:38 +0300

    update

--- application.py
+++ application.py
@@ -1,18 +1,12 @@
 import os
-
 from flask import Flask, render_template, request, send_file
-from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from models import *
 
 app = Flask(__name__)
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-engine = create_engine('mssql://KEVINKAGWIMA/alphataji?driver=sql server?trusted_connection=yes')
-db = scoped_session(sessionmaker(bind=engine))
+app.config["SQLALCHEMY_DATABASE_URI"] = ('mssql://KEVINKAGWIMA/alphataji?driver=sql server?trusted_connection=yes')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 @app.route("/")
 @app.route("/home")
@@ -49,20 +43,34 @@ def signup():
 
 @app.route("/register", methods=["POST"])
 def register():
-  fname = request.form.get("fname")
-  sname = request.form.get("sname")
-  lname = request.form.get("lname")
+  firstname = request.form.get("fname")
+  secondname = request.form.get("sname")
+  lastname = request.form.get("lname")
   email = request.form.get("email")
   phone = request.form.get("phone")
   username = request.form.get("username")
   password = request.form.get("password")
-
-  if db.execute("SELECT username, email, phone_number FROM members WHERE username=:username or email=:email or phone_number=:phone", {"username":username, "email":email, "phone":phone}).rowcount != 0:
-    return render_template("signup.html", message="Email, phone number or username already exists. Try again")
   
-  db.execute("INSERT INTO members (first_name, second_name, last_name, email, phone_number, username, password) VALUES (:first_name, :second_name, :last_name, :email, :phone_number, :username, :password)", {"first_name":fname, "second_name":sname, "last_name":lname, "email":email, "phone_number":phone, "username":username, "password":password})
+  member = (
+    user(first_name=firstname,second_name=secondname,last_name=lastname,email=email,phone_number=phone,username=username,password=password
+    )
+  )
 
-  db.commit()
+  email_exists = db.session.query(user).filter_by(email=email).first()
+  if email_exists is not None:
+    return render_template("signup.html", message="Email already exists. Try again")
+
+  phonenumber_exists = db.session.query(user).filter_by(phone_number=phone).first()
+  if phonenumber_exists is not None:
+    return render_template("signup.html", message="Phone number already exists. Try again")
+  
+  username_exists = db.session.query(user).filter_by(username=username).first()
+  if username_exists is not None:
+    return render_template("signup.html", message="Username already exists. Try again")
+
+  db.session.add(member)
+  
+  db.session.commit()
   return render_template("signin.html", success="User registred.")
 
 @app.route("/signin")
@@ -74,52 +82,57 @@ def authenticate():
   username = request.form.get("username")
   password = request.form.get("password")
 
-  if db.execute("SELECT username, password FROM members WHERE username=:username and password=:password", {"username":username, "password":password}).rowcount == 0:
-    return render_template("signin.html", message="Invalid Credentials")
+  check_username = db.session.query(user.username).filter_by(username=username).first()
+  if check_username is None:
+    return render_template("signin.html", message="Invalid username")
+
+  check_password = db.session.query(user.username).filter_by(password=password).first()
+  if check_password is None:
+    return render_template("signin.html", message="Invalid password")
 
   return render_template("index.html", name=username, message="logged in as")
 
 @app.route('/download')
 def download_file():
-  path = "branch-loan-form.pdf"
+  path = "Loan Forms/branch-loan-form.pdf"
   return send_file(path, as_attachment=True)
 
 @app.route('/download2')
 def download_file_2():
-  path = "golden-loan-form.pdf"
+  path = "Loan Forms/golden-loan-form.pdf"
   return send_file(path, as_attachment=True)
 
 @app.route('/download3')
 def download_file_3():
-  path = "express-loan-form.pdf"
+  path = "Loan Forms/express-loan-form.pdf"
   return send_file(path, as_attachment=True)
 
 @app.route('/download4')
 def download_file_4():
-  path = "normal-loan-form.pdf"
+  path = "Loan Forms/normal-loan-form.pdf"
   return send_file(path, as_attachment=True)
 
 @app.route('/download5')
 def download_file_5():
-  path = "supersaver-loan-form.pdf"
+  path = "Loan Forms/supersaver-loan-form.pdf"
   return send_file(path, as_attachment=True)
 
 @app.route('/download6')
 def download_file_6():
-  path = "super-loan-form.pdf"
+  path = "Loan Forms/super-loan-form.pdf"
   return send_file(path, as_attachment=True)
 
 @app.route('/download7')
 def download_file_7():
-  path = "msingi-loan-form.pdf"
+  path = "Loan Forms/msingi-loan-form.pdf"
   return send_file(path, as_attachment=True)
 
 @app.route('/download8')
 def download_file_8():
-  path = "dividends advance-loan-form.pdf"
+  path = "Loan Forms/dividends advance-loan-form.pdf"
   return send_file(path, as_attachment=True)
 
 @app.route('/download9')
 def download_file_9():
-  path = "benovelent fund-form.pdf"
+  path = "Loan Forms/benovelent fund-form.pdf"
   return send_file(path, as_attachment=True)
